6f3ef869461cf960d3e1820b704522490690845b
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-03-03 15:44:46 +0300

    first changes

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import pino from 'pino-http';
+import cors from 'cors';
+import express from 'express';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
