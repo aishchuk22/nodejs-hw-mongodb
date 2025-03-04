83de33c0e60988817f309f34103d21bd94bb1ab5
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-03-04 06:29:29 +0300

    done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import express from 'express';
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -17,6 +18,32 @@ export const startServer = () => {
       },
     }),
   );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
