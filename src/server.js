87ad791df78a45af43112c8f2ac0aad04eb430b2
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-03-04 11:51:11 +0300

    update

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ export const startServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -34,12 +35,13 @@ export const startServer = () => {
 
     if (!contact) {
       res.status(404).json({
-        message: 'Student not found',
+        message: 'Contact not found',
       });
       return;
     }
 
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
