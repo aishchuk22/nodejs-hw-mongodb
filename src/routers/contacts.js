6c5f13687997150a05a68346f066d54f20d86722
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-03-16 20:46:37 +0300

    validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,28 +9,45 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  upsertContactSchema,
+} from '../validation/contacts.js';
+
 const contactsRouter = Router();
 
 contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 contactsRouter.get(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
-contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+contactsRouter.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
 contactsRouter.delete(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(deleteContactController),
 );
 
 contactsRouter.put(
   '/contacts/:contactId',
+  isValidId,
+  validateBody(createContactSchema),
   ctrlWrapper(upsertContactController),
 );
 
 contactsRouter.patch(
   '/contacts/:contactId',
+  isValidId,
+  validateBody(upsertContactSchema),
   ctrlWrapper(patchContactController),
 );
 
